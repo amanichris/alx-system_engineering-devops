009bf48c15709dda6e66bce9edab28cdb6707eb1
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2024-04-24 22:24:40 +0100

    file created

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,14 +1,7 @@
 # The code will create a file
 file { '/tmp/school':
-	ensure	=> file,
-	content => 'I love Puppet',
-	mode	=> '07744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
+	mode 	=> '0744',
+	owner 	=> 'www-data',
+	group 	=> 'www-data',
+	content => 'I love Puppet'
 }
-
-
-
-
-
-
